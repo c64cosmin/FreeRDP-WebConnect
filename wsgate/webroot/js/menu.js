46f0de4fe91be17cd4170970f3627ddee1b84f6d
Cosmin Munteanu <cmunteanu@cloudbasesolutions.com>
2015-12-16 17:44:56 +0200

    Added input area

--- wsgate/webroot/js/menu.js
+++ wsgate/webroot/js/menu.js
@@ -8,8 +8,8 @@ var LoginMenu = function(){
     this.xTarget = 0;
     this.x = 0;
     this.y = window.innerHeight*(1.0 - this.height)*0.5;
-    this.buttonHeight = 40;
-    this.state = "centered";
+    this.buttonHeight = 30;
+    this.state = "center";
     this.elem.style["position"] = "absolute";
     this.elem.style["width"] = window.innerWidth * this.width + "px";
     this.elem.style["height"] = window.innerHeight * this.height + "px";
@@ -17,20 +17,25 @@ var LoginMenu = function(){
     this.elem.style["top"] = this.y + "px";
     this.elem.style["overflow"] = "hidden";
     this.entries = [];
-    this.entries.push(new TextEntry(this.elem, "Woot"));
-    this.entries.push(new TextEntry(this.elem, "Woot"));
+    this.entries.push(new TextEntry(this.elem, "test0", "Woot"));
+    this.entries.push(new TextEntry(this.elem, "test1", "Woot"));
     this.update = function(){
-        if(this.state == "centered"){
+        if(this.state == "center"){
             this.heightTarget = (this.entries.length * this.buttonHeight) / window.innerHeight;
-            this.xTarget = window.innerWidth * (1-this.width) * 0.5
+            this.xTarget = window.innerWidth * (1-this.width) * 0.5;
         }
         if(this.state == "left"){
             this.heightTarget = 1;
             this.xTarget = 0;
         }
+        if(this.state == "right"){
+            this.heightTarget = 1;
+            this.xTarget = window.innerWidth * (1-this.width);
+        }
+        this.height += (this.heightTarget - this.height)*0.1;
         this.x += (this.xTarget - this.x)*0.1;
         this.y = window.innerHeight*(1.0 - this.height)*0.5;
-        this.height += (this.heightTarget - this.height)*0.1;
+        this.elem.style["width"] = window.innerWidth * this.width + "px";
         this.elem.style["height"] = window.innerHeight * this.height + "px";
         this.elem.style["left"] = this.x + "px";
         this.elem.style["top"] = this.y + "px";
@@ -40,21 +45,42 @@ var LoginMenu = function(){
     }
 }
 
-var TextEntry = function(parent, caption){
+var TextEntry = function(parent, id, caption){
     this.elem = document.createElement("div");
     parent.appendChild(this.elem);
+    this.id = id;
     this.height = 0;
     this.caption = caption;
     this.elem.style["position"] = "relative";
     this.elem.style["width"] = "100%";
     this.elem.style["height"] = "0px";
-    this.elem.style["background-color"] = "#ff0000";
     this.elem.style["margin-bottom"] = "1px";
     this.elem.style["overflow"] = "hidden";
     this.elem.className = "textentry";
-    this.elem.innerHTML = caption;
     this.update = function(pixelHeight){
         this.height += (pixelHeight - 1 - this.height)*0.1;
         this.elem.style["height"] = this.height + "px";
     }
+    this.createCaption = function(){
+        this.captionElem = document.createElement("div");
+        this.elem.appendChild(this.captionElem);
+        this.captionElem.style["position"] = "relative";
+        this.captionElem.style["width"] = "25%";
+        this.captionElem.style["height"] = "100%";
+        this.captionElem.style["display"] = "table";
+        this.captionElem.style["float"] = "left";
+        this.captionElem.innerHTML = "<p class='textareacaption' style='display:table-cell;vertical-align:middle;text-align:center'>" + this.caption + "</p>";
+    }
+    this.createTextArea = function(){
+        this.textAreaElem = document.createElement("div");
+        this.elem.appendChild(this.textAreaElem);
+        this.textAreaElem.style["position"] = "relative";
+        this.textAreaElem.style["position"] = "relative";
+        this.textAreaElem.style["width"] = "75%";
+        this.textAreaElem.style["height"] = "100%";
+        this.textAreaElem.style["float"] = "right";
+        this.textAreaElem.innerHTML = "<input id='" + this.id + "' style='width:100%;height:100%;resize:none;margin:1px;float:right;'></input>";
+    }
+    this.createCaption();
+    this.createTextArea();
 }
